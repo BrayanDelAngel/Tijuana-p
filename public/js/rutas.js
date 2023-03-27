b7042c077741bdff03b03370c8d9546c7d3943e5
CIIGA <ciigasistemas@gmail.com>
2023-03-27 11:01:19 -0600

    Arreglos 2

--- public/js/rutas.js
+++ public/js/rutas.js
@@ -1,25 +1,25 @@
 
 function determinacion(cuenta) {
-    var contenedor = document.getElementById('contenedor_carga');
-    contenedor.style.visibility = '';
-    contenedor.style.opacity = '0.9';
+    // var contenedor = document.getElementById('contenedor_carga');
+    // contenedor.style.visibility = '';
+    // contenedor.style.opacity = '0.9';
     window.location.href = "/calculo/" + cuenta;
 }
 function requerimiento(cuenta) {
-    var contenedor = document.getElementById('contenedor_carga');
-    contenedor.style.visibility = '';
-    contenedor.style.opacity = '0.9';
+    // var contenedor = document.getElementById('contenedor_carga');
+    // contenedor.style.visibility = '';
+    // contenedor.style.opacity = '0.9';
     window.location.href = "/formR/" + cuenta;
 }
 function mandamiento(cuenta) {
-    var contenedor = document.getElementById('contenedor_carga');
-    contenedor.style.visibility = '';
-    contenedor.style.opacity = '0.9';
+    // var contenedor = document.getElementById('contenedor_carga');
+    // contenedor.style.visibility = '';
+    // contenedor.style.opacity = '0.9';
     window.location.href = "/formM/" + cuenta;
 }
 function pdf(cuenta) {
-    var contenedor = document.getElementById('contenedor_carga');
-    contenedor.style.visibility = '';
-    contenedor.style.opacity = '0.9';
+    // var contenedor = document.getElementById('contenedor_carga');
+    // contenedor.style.visibility = '';
+    // contenedor.style.opacity = '0.9';
     window.location.href = "/pdf/" + cuenta;
 }
