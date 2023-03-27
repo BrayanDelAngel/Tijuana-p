e0fcc2482e9f0d63a7228ef84531aea36da50c74
CIIGA <ciigasistemas@gmail.com>
2023-03-27 11:04:06 -0600

    cambios

--- public/js/rutas.js
+++ public/js/rutas.js
@@ -6,20 +6,20 @@ function determinacion(cuenta) {
     window.location.href = "https://gallant-driscoll.198-71-62-113.plesk.page/implementta/modulos/Tijuana-p/public/calculo/" + cuenta;
 }
 function requerimiento(cuenta) {
-    var contenedor = document.getElementById('contenedor_carga');
-    contenedor.style.visibility = '';
-    contenedor.style.opacity = '0.9';
+    // var contenedor = document.getElementById('contenedor_carga');
+    // contenedor.style.visibility = '';
+    // contenedor.style.opacity = '0.9';
     window.location.href = "https://gallant-driscoll.198-71-62-113.plesk.page/implementta/modulos/Tijuana-p/public/formR/" + cuenta;
 }
 function mandamiento(cuenta) {
-    var contenedor = document.getElementById('contenedor_carga');
-    contenedor.style.visibility = '';
-    contenedor.style.opacity = '0.9';
+    // var contenedor = document.getElementById('contenedor_carga');
+    // contenedor.style.visibility = '';
+    // contenedor.style.opacity = '0.9';
     window.location.href = "https://gallant-driscoll.198-71-62-113.plesk.page/implementta/modulos/Tijuana-p/public/formM/" + cuenta;
 }
 function pdf(cuenta) {
-    var contenedor = document.getElementById('contenedor_carga');
-    contenedor.style.visibility = '';
-    contenedor.style.opacity = '0.9';
+    // var contenedor = document.getElementById('contenedor_carga');
+    // contenedor.style.visibility = '';
+    // contenedor.style.opacity = '0.9';
     window.location.href = "https://gallant-driscoll.198-71-62-113.plesk.page/implementta/modulos/Tijuana-p/public/pdf/" + cuenta;
 }
